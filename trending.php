950ff4b8916b83e34eb7bacaf46f8eb7ae3d8894
Bryan <bryanwan23@gmail.com>
2013-09-25 14:04:41 -0400

    trending page

--- trending.php
+++ trending.php
@@ -22,16 +22,37 @@ $userid = $_SESSION['userid'];
     <body>
         <img src="/img/loading.gif" id="loading"/>
         <?php commonHeader(); ?>
+        <div id="mainContainer">
 
-        <div id="trendContainer">
-            <?php
-            $tagsQuery = "SELECT * FROM tag ORDER BY count LIMIT 10";
-            $tagsResult = mysql_query($tagsQuery);
-            
-            while ($tag = mysql_fetch_array($tagsResult)) {
-                echo $tag['name'];
-            }
-            ?>
+            <div id="topLabel"><span id="topText">Trending Tags</span></div>
+
+            <div id="topContainer" style="top:210px;">
+                <div id="followers" class="previewContainer" style="display:none;">
+                    <br/>
+                    <div class="linedTitle">
+                        <span class="linedText">
+                            Tags
+                        </span>
+                    </div>
+                    <br/>
+                    <?php
+                    $tagsQuery = "SELECT * FROM tag ORDER BY count DESC LIMIT 10";
+                    $tagsResult = mysql_query($tagsQuery);
+                    while ($tag = mysql_fetch_array($tagsResult)) {
+                        echo $tag['name'];
+                    }
+                    ?>
+                </div>
+            </div>
+            <div id="itemBackground">
+                <div class="divider">
+                    <hr class="left"/>
+                    <span id="mainHeading">THE HIVE</span>
+                    <hr class="right" />
+                </div>
+                <button id="loadMore" class="greenButton"  onclick="itemPagination();">Load More...</button>
+
+            </div>
         </div>
     </body>
 </html>
