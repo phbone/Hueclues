f286ad88190869363aeb7851104538aabfa94941
Bryan <bryanwan23@gmail.com>
2013-09-27 17:56:59 -0400

    extraction notification

--- saveitem_processing.php
+++ saveitem_processing.php
@@ -52,11 +52,11 @@ if ($code != "" && $desc != "") { // all fields filled in
     }
 
     database_increment("user", "userid", $userid, "itemcount", 1); // increases the number of swatches saved
-    $_SESSION['save_notification'] = "<span id='success_message'><br>Item successfully added to closet! <br/><a class='notificationLinks' href='/closet'>See It In Closet</a></span><br><a class='notificationLinks' onclick='addMore()'>Add More</a></span>";
+    $_SESSION['save_notification'] = "<span id='success_message'><br>Item added to closet! <br/><a class='notificationLinks' href='/closet'>See It In Closet</a><br><a class='notificationLinks' onclick='addMore()'>Add More</a></span>";
 } else if ($code == "") {
-    $_SESSION['save_notification'] = "<span id='error_message'>Please select an item color by clicking the uploaded image.</span>";
+    $_SESSION['save_notification'] = "<br><br><span id='error_message'>Please select an item color by clicking the uploaded image.</span>";
 } else if ($desc == "") {
-    $_SESSION['save_notification'] = "<span id='error_message'>Please describe the item in the space provided.</span>";
+    $_SESSION['save_notification'] = "<br><br><span id='error_message'>Please describe the item in the space provided.</span>";
 }
 
 for ($i = 0; $i < count($tags_array); $i++) {
