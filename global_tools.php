99f2528fe3a6bf882840a9f61ee960fc970910df
Bryan <bryanwan23@gmail.com>
2013-09-27 20:24:45 -0400

    beta sign up

--- global_tools.php
+++ global_tools.php
@@ -13,6 +13,31 @@ function getImagetype($imageType) {
     return $imageType;
 }
 
+function isPrime($num) {
+    if ($num == 1)
+        return false;
+    //2 is prime (the only even number that is prime)
+    if ($num == 2)
+        return true;
+    /**
+     * if the number is divisible by two, then it's not prime and it's no longer
+     * needed to check other even numbers
+     */
+    if ($num % 2 == 0) {
+        return false;
+    }
+    /**
+     * Checks the odd numbers. If any of them is a factor, then it returns false.
+     * The sqrt can be an aproximation, hence just for the sake of
+     * security, one rounds it to the next highest integer value.
+     */
+    for ($i = 3; $i <= ceil(sqrt($num)); $i = $i + 2) {
+        if ($num % $i == 0)
+            return false;
+    }
+    return true;
+}
+
 function emailTemplate($message) {
     return "<html><head></head><body><table width='100%' border='0' cellspacing='0' cellpadding='0' bgcolor='#eeeeee'>
             <tr><td align='center'>
@@ -227,7 +252,6 @@ function socialMedia() {
     echo "https://www.facebook.com/sharer/sharer.php?t=hueclues&u=http://hueclues.com/closet/thesunnyos";
 }
 
-
 function returnAllItemsFromFollowing($user_id, $field = "") {
     // returns item objects from all of the people $user_id is following
     $followingArray = Array();
@@ -249,8 +273,6 @@ function returnAllItemsFromFollowing($user_id, $field = "") {
     }
     return $followingItems;
 }
-
-
 
 function formatItem($userid, $item_object, $height = "") {
     $owns_item = ($userid == $item_object->owner_id);
