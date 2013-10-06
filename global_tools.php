86b9bd4ccbcb231017f528ca56558c20f32c9fa7
Bryan <bryanwan23@gmail.com>
2013-10-06 17:28:32 -0400

    meta tags

--- global_tools.php
+++ global_tools.php
@@ -129,7 +129,8 @@ function initiateTools() {
 }
 
 function metaTags(){
-    echo "<meta property='og:image' content='http://hueclues.com/img/hc_icon_new.png'/>
+    echo "<meta name='description' content='Pollinate Fashion With The World'> 
+<meta property='og:image' content='http://hueclues.com/img/hc_icon_new.png'/>
      <meta property='og:title' content='Hueclues'/>
      <meta property='og:url' content='hueclues.com'/>
      <meta property='og:site_name' content='hueclues - the fashion hub'/>
