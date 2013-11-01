428d6fbdb73b93624c851bd860b80e6699fc509e
Bryan <bryanwan23@gmail.com>
2013-10-31 23:23:20 -0400

    blank page

--- global_objects.php
+++ global_objects.php
@@ -1,5 +1,76 @@
 <?php
 
+class item_object {
+
+    public $owner_id = "";
+    public $owner_name = "";
+    public $owner_username = "";
+    public $owner_picture = "";
+    public $owner_followers = "";
+    public $purchaselink = "";
+    public $itemid = "";
+    public $image_link = "";
+    public $hexcode = "";
+    public $tags = "";
+    public $description = "";
+    public $save_time = "";
+    public $image_origin = "";
+    public $search_string = "";
+    public $item_tags_string = "";
+    public $association = "";
+    public $matchScheme = "";
+
+}
+
+class store_match_object {
+
+    // the itemid of the store item
+    // the priority, i.e degree of match
+    // 1 being lowest (1 color matches) 3 being highest (3 colors match)
+    public $itemid = "";
+    public $description = "";
+    public $gender = "";
+    public $purchaselink = "";
+    public $colors = array();
+    public $url = "";
+    public $priority = "";
+    public $price = "";
+    public $scheme = ""; // ana, tri, comp, spl, sha
+
+}
+
+class colorObject {
+
+    // the itemid of the store item
+    // the priority, i.e degree of match
+    // 1 being lowest (1 color matches) 3 being highest (3 colors match)
+    public $hex = "";
+    public $comp = "";
+    public $ana1 = "";
+    public $ana2 = "";
+    public $spl1 = "";
+    public $spl2 = "";
+    public $tri1 = "";
+    public $tri2 = "";
+    public $sha1 = "";
+    public $sha2 = "";
+
+}
+
+class matchObject {
+
+    // match object returns itemid with associations:
+    //      whether the item is 
+    //      from following, closet, or store
+
+    public $priority = ""; // how well item matches
+    public $matchingItemid = "";
+    public $itemSource = ""; // closet, following, or store
+    public $scheme = ""; // analogous(ana), complimentary(comp)
+
+    // shades(sha), split(spl), triadic (tri)
+}
+
 function returnItem($itemid) {
 //// Input: itemid INT
 //// given an itemid, return an object with the
@@ -297,77 +368,6 @@ function colorsMatching($hex) {
     $colorObj->sha2 = $tints[4];
     $colorObj->hex = $hex;
     return $colorObj;
-}
-
-class item_object {
-
-    public $owner_id = "";
-    public $owner_name = "";
-    public $owner_username = "";
-    public $owner_picture = "";
-    public $owner_followers = "";
-    public $purchaselink = "";
-    public $itemid = "";
-    public $image_link = "";
-    public $hexcode = "";
-    public $tags = "";
-    public $description = "";
-    public $save_time = "";
-    public $image_origin = "";
-    public $search_string = "";
-    public $item_tags_string = "";
-    public $association = "";
-    public $matchScheme = "";
-
-}
-
-class store_match_object {
-
-    // the itemid of the store item
-    // the priority, i.e degree of match
-    // 1 being lowest (1 color matches) 3 being highest (3 colors match)
-    public $itemid = "";
-    public $description = "";
-    public $gender = "";
-    public $purchaselink = "";
-    public $colors = array();
-    public $url = "";
-    public $priority = "";
-    public $price = "";
-    public $scheme = ""; // ana, tri, comp, spl, sha
-
-}
-
-class colorObject {
-
-    // the itemid of the store item
-    // the priority, i.e degree of match
-    // 1 being lowest (1 color matches) 3 being highest (3 colors match)
-    public $hex = "";
-    public $comp = "";
-    public $ana1 = "";
-    public $ana2 = "";
-    public $spl1 = "";
-    public $spl2 = "";
-    public $tri1 = "";
-    public $tri2 = "";
-    public $sha1 = "";
-    public $sha2 = "";
-
-}
-
-class matchObject {
-
-    // match object returns itemid with associations:
-    //      whether the item is 
-    //      from following, closet, or store
-
-    public $priority = ""; // how well item matches
-    public $matchingItemid = "";
-    public $itemSource = ""; // closet, following, or store
-    public $scheme = ""; // analogous(ana), complimentary(comp)
-
-    // shades(sha), split(spl), triadic (tri)
 }
 ?> 
 
