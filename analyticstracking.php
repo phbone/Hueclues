0c17d1cab523be2e2c28d96ac85a06ed4ecd6194
headleybrown <dannyisbrown@Danny-Browns-MacBook-Pro.local>
2013-11-11 13:32:25 -0500

    analyticspage fix

--- analyticstracking.php
+++ analyticstracking.php
@@ -1,11 +1,6 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
-<script>
+echo "<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -13,5 +8,6 @@
 
   ga('create', 'UA-45618707-1', 'hueclues.com');
   ga('send', 'pageview');
-
-</script>
+</script>";
+ 
+?>
--- history.php
+++ history.php
@@ -247,6 +247,7 @@ $loginUrl = $facebook->getLoginUrl($params);
         </script>
     </head>
     <body>
+        <?php include_once("analyticstracking.php") ?>
         <?php initiateNotification() ?>
         <img src="/img/loading.gif" id="loading" />
         <?php commonHeader(); ?>
