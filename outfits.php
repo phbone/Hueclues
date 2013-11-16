1499a173d6806ec6bbccb42d9c7e81c5d7e3408f
Bryan <bryanwan23@gmail.com>
2013-11-16 17:19:47 -0500

    outfit extra spacing

--- outfits.php
+++ outfits.php
@@ -108,6 +108,7 @@ $outfit = database_fetch("outfit", "outfitid", $current_outfitid);
                 display:block;
                 position:absolute;
                 width:100%;
+                z-index:1;
             }
         </style>
     </head>
