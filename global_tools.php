f275824cc7309f312679fb670fa63c687fbc51fa
Bryan <bryanwan23@gmail.com>
2013-11-16 19:34:48 -0500

    change size

--- global_tools.php
+++ global_tools.php
@@ -307,9 +307,9 @@ function formatOutfitItem($userid, $itemObject, $height = "", $itemLink = "") { 
             $itemLink = "/hue/" . $itemObject->itemid;
         }
         $itemLink = "/hue/" . $itemObject->itemid;
-        echo "<div class='outfitItemContainer' id='item" . $itemObject->itemid . "'style='color:#" . $itemObject->text_color . ";height:" . (($height) ? $height . "px;width:auto" : "") . "' >
+        echo "<div class='outfitItemContainer' id='item" . $itemObject->itemid . "'style='color:#" . $itemObject->text_color . ";' >
     <span class = 'itemDescription' style='background-color:#" . $itemObject->hexcode . ";width:auto'>" . stripslashes($itemObject->description) . "</span>
-        <img alt = '  This Image Is Broken' src = '" . $itemObject->image_link . "' onclick=\"Redirect('$itemLink')\" class = 'fixedwidththumb thumbnaileffect' style='height:inherit;width:auto' />
+        <img alt = '  This Image Is Broken' src = '" . $itemObject->image_link . "' onclick=\"Redirect('$itemLink')\" class = 'fixedwidththumb thumbnaileffect' style='height:" . (($height) ? $height . "px;width:auto" : "") . "' />
     <div class='itemTagBox' style='background-color:#" . $itemObject->hexcode . ";overflow-x:hidden;width:100%'>
         <input type = 'text' class='itemTag'  name = 'tags'" . ((!$owns_item) ? "readonly = 'true'" : "") . " onchange = 'updateTags(this, " . $itemObject->itemid . ")' value = '" . $item_tags_string . "' placeholder = 'define this style with #hashtags' />
         <input type = 'text' class='purchaseLink'  name = 'purchaseLink' onblur='hidePurchaseLink(" . $itemObject->itemid . ")' onchange = 'updatePurchaseLink(this, " . $itemObject->itemid . ")' value = '" . $itemObject->purchaselink . "' placeholder = 'link to buy/find item' />     
