cd040b0a645af9e73dfc9f7406257d82ea31d8d0
Bryan <bryanwan23@gmail.com>
2013-11-21 17:25:40 -0500

    add to outfit now shows when you add it

--- js/global_javascript.js
+++ js/global_javascript.js
@@ -431,7 +431,7 @@ function addToOutfit(itemid) {
             addObject = jQuery.parseJSON(html);
             if (addObject.notification == "success") {
                 console.log("success message reached, problem with notification setup");
-                loadOutfit();
+                toggleOutfit("show");
 //var notification = "This item was added to your current outfit!<br/><a href='/outfits'>Go To Outfits</a>";
                 //$("#notification").html(notification);
                 //displayNotification(notification);
