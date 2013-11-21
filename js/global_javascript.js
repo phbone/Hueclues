876dc4b5d21a19b93372e4c20cb4bd3de5e872e6
Bryan <bryanwan23@gmail.com>
2013-11-20 22:31:23 -0500

    inline item container

--- js/global_javascript.js
+++ js/global_javascript.js
@@ -79,7 +79,6 @@ function formatOutfitItems(userid, itemObject) {
     var lockString = "readonly='true'";
 
     $('#outfitBar').append("<div class='outfitItemContainer' id='item" + itemObject.itemid + "' style='color:#" + itemObject.text_color + ";height:125px;'>\n\
-<span class = 'outfitDescription' style='background-color:#" + itemObject.hexcode + "'>" + stripslashes(itemObject.description) + "</span>\n\
 <img alt = '  This Image Is Broken' class='outfitImage' src = '" + itemObject.image_link + "' onclick='Redirect(\"/hue/" + itemObject.itemid + "\")'/>\n\
 <div class='outfitItemTagBox' style='background-color:#" + itemObject.hexcode + "'>\n\
 <input type = 'text' class='itemTag'  name = 'tags'" + lockString + "onchange = 'updateTags(this, " + itemObject.itemid + ")' value = '" + itemObject.tags + "' placeholder = 'define this style with #hashtags' />\n\
