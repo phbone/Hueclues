04ed82eeb8c594b6a22b2174359f1cbbeb87df88
Bryan <bryanwan23@gmail.com>
2013-11-25 01:05:27 -0500

    view all button format

--- js/global_javascript.js
+++ js/global_javascript.js
@@ -437,7 +437,7 @@ function loadOutfit() {// reloads outfit
 <button class = 'greenButton' id = 'deleteOutfitButton' title='delete this outfit' onclick = 'deleteOutfit()'>X</button>\n\
 <button class='greenButton' id='saveOutfitButton'>Save</button>\n\
 <button class='greenButton' id='createOutfitNavButton' onclick='createOutfit()'>New Outfit</button>\n\
-<button class='greenButton' id='viewAllOutfitButton' onclick='Redirect('/closet/view/')'>\n\
+<button class='greenButton' id='viewAllOutfitButton' onclick='Redirect('/closet/view/')'>View All</button>\n\
 </div>");
             }
             $("#loading").hide();
