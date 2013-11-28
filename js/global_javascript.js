0bd63ffbe54331af95829ac78f87a1c624946a33
Bryan <bryanwan23@gmail.com>
2013-11-27 21:14:46 -0500

    remove / after outfit

--- js/global_javascript.js
+++ js/global_javascript.js
@@ -452,7 +452,7 @@ function loadOutfit() {// reloads outfit
 <button class = 'greenButton' id = 'deleteOutfitButton' title='delete this outfit' onclick = 'deleteOutfit()'>X</button>\n\
 <button class='greenButton' id='saveOutfitButton' onclick='saveOutfit()'>Save</button>\n\
 <button class='greenButton' id='createOutfitNavButton' onclick='createOutfit()'>New Outfit</button>\n\
-<button class='greenButton' id='viewAllOutfitButton' onclick='Redirect(\"/closet/" + username + "/outfit/\")'>View All</button>\n\
+<button class='greenButton' id='viewAllOutfitButton' onclick='Redirect(\"/closet/" + username + "/outfit\")'>View All</button>\n\
 </div>");
             }
             $("#loading").hide();
