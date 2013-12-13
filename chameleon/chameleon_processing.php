329cc375c166befeb406509226d29f81f77b2adf
Bryan <bryanwan23@gmail.com>
2013-12-13 16:46:21 -0500

    chameleon processing.php

--- chameleon/chameleon_processing.php
+++ chameleon/chameleon_processing.php
@@ -9,16 +9,20 @@ $csv = explode(",", $csv);
 $i = 0;
 $count = count($csv);
 
-echo $password;
-print_r($csv);
+
 if ($password == "wellshieeet") {
     
     while ($i < $count) {
         //url, description, price, gender, code1, code2, code3, purchaselink
-        if (database_count("storeitem", "url", $csv[$i], "description", $csv[$i + 1])<1) {
+        
+        if (database_count("storeitem", "url", $csv[$i], "description", $csv[$i + 1])<1) {// check repeats
             database_insert("storeitem", "url", $csv[$i], "description", $csv[$i + 1], "price", $csv[$i + 2], "gender", $csv[$i + 3], "code1", $csv[$i + 4], "code2", $csv[$i + 5], "code3", $csv[$i + 6], "purchaselink", $csv[$i + 7]);
-            $i+=8;
+            
         }
+        else{
+            echo "repeat caught<br/>";
+        }
+        $i+=8;
     }
     echo "inserted " . ($i / 8) . "items";
 } else {
