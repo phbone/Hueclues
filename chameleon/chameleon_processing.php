9c656e73991b1630a709b1b80302e74c03dd4d3d
Bryan <bryanwan23@gmail.com>
2013-12-13 16:55:15 -0500

    chameleon

--- chameleon/chameleon_processing.php
+++ chameleon/chameleon_processing.php
@@ -12,12 +12,12 @@ $count = count($csv);
 
 if ($password == "wellshieeet") {
     
+    echo $count;
     while ($i < $count) {
         //url, description, price, gender, code1, code2, code3, purchaselink
-        
+        echo " inside while loop";
         if (database_count("storeitem", "url", $csv[$i], "description", $csv[$i + 1])<1) {// check repeats
             database_insert("storeitem", "url", $csv[$i], "description", $csv[$i + 1], "price", $csv[$i + 2], "gender", $csv[$i + 3], "code1", $csv[$i + 4], "code2", $csv[$i + 5], "code3", $csv[$i + 6], "purchaselink", $csv[$i + 7]);
-            
         }
         else{
             echo "repeat caught<br/>";
