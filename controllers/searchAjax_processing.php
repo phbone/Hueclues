04593ceab1481c7fa910bef29ef416329570435d
Bryan <bryanwan23@gmail.com>
2014-01-17 14:17:21 -0500

    ajax 15 results

--- controllers/searchAjax_processing.php
+++ controllers/searchAjax_processing.php
@@ -9,18 +9,20 @@ include('../database_functions.php');
 $query = $_POST['q'];
 $queryWord = str_replace("#", "", $query);
 $searchArray = array();
-
+$count = 0; // only return up to 15 results
 if (preg_match('/#/', $query)) {
     // hashtag search
-    $searchResults = database_like_results("tag", "name", $queryWord, 15);
-    while ($tag = mysql_fetch_array($searchResults)) {
+    $searchResults = database_like_results("tag", "name", $queryWord);
+    while ($tag = mysql_fetch_array($searchResults) && $count < 15) {
         $searchArray[] = "#" . $tag['name'] . "(" . $tag['count'] . ")";
+        $count++;
     }
 } else {
     // user search
-    $searchResults = database_like_results("user", "username", $queryWord, 15);
-    while ($user = mysql_fetch_array($searchResults)) {
+    $searchResults = database_like_results("user", "username", $queryWord);
+    while ($user = mysql_fetch_array($searchResults) && $count < 15) {
         $searchArray[] = $user['username'];
+        $count++;
     }
 }
 
