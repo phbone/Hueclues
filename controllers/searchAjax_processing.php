6079979c9586148be1780a931af4b2481e2bb1e2
Bryan <bryanwan23@gmail.com>
2014-01-17 14:25:25 -0500

    ajax 15 results max return

--- controllers/searchAjax_processing.php
+++ controllers/searchAjax_processing.php
@@ -13,14 +13,14 @@ $count = 0; // only return up to 15 results
 if (preg_match('/#/', $query)) {
     // hashtag search
     $searchResults = database_like_results("tag", "name", $queryWord);
-    while ($tag = mysql_fetch_array($searchResults) && $count < 15) {
+    while (($tag = mysql_fetch_array($searchResults)) && $count < 15) {
         $searchArray[] = "#" . $tag['name'] . "(" . $tag['count'] . ")";
         $count++;
     }
 } else {
     // user search
     $searchResults = database_like_results("user", "username", $queryWord);
-    while ($user = mysql_fetch_array($searchResults) && $count < 15) {
+    while (($user = mysql_fetch_array($searchResults)) && $count < 15) {
         $searchArray[] = $user['username'];
         $count++;
     }
