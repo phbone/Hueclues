334b0cb33f3e801910fbc38c8bb59668453f8ecb
Bryan <bryanwan23@gmail.com>
2014-01-26 23:04:04 -0500

    query

--- models/sting_functions.php
+++ models/sting_functions.php
@@ -8,9 +8,11 @@
 function stingColor($hexcode){
  // find all clothing attached to the input color
     $h = str_split($hexcode);
-    $stingRst = mysql_query("SELECT * FROM item WHERE code LIKE '".$h[0]."%".$h[2]."%".$h[4]."%'");
-    while ($item = mysql_fetch_array(mysql_query)){
-        formatItem(returnItem($item['itemid']));
+    $stingRst = mysql_query("SELECT * FROM item WHERE code LIKE '%$h[0]_$h[2]_$h[4]_%'");
+    while ($item = mysql_fetch_array($stingRst)){
+        $itemObject = new item_object;
+        $itemObject = returnItem($item['itemid']);
+        formatItem($itemObject);
     }
     
 }
