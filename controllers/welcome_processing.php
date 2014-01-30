a4bab371dc85ffb19caca2695689188312e596c9
Bryan <bryanwan23@gmail.com>
2014-01-30 13:05:50 -0500

    welcome increment follower count for super closets

--- controllers/welcome_processing.php
+++ controllers/welcome_processing.php
@@ -16,18 +16,22 @@ if ($gender == "m") {
     database_update("user", "userid", $userid, "", "", "gender", "1");
     if (!database_count("follow", "userid", "1", "followerid", $userid)) {
         database_insert("follow", "userid", "1", "followerid", $userid);
+        database_increment("user", "userid", "1", "followers", 1);
     }
     if (!database_count("follow", "userid", "2", "followerid", $userid)) {
         database_insert("follow", "userid", "2", "followerid", $userid);
+        database_increment("user", "userid", "2", "followers", 1);
     }
 } else if ($gender == "f") {
     // default is set to 0 so just follow female closets
 
     if (!database_count("follow", "userid", "3", "followerid", $userid)) {
         database_insert("follow", "userid", "3", "followerid", $userid);
+        database_increment("user", "userid", "3", "followers", 1);
     }
     if (!database_count("follow", "userid", "4", "followerid", $userid)) {
         database_insert("follow", "userid", "4", "followerid", $userid);
+        database_increment("user", "userid", "4", "followers", 1);
     }
 }
 
