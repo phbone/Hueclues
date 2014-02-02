f6cc04d466a0d132b1a5dc4c5d5323b1d718e1d5
Bryan <bryanwan23@gmail.com>
2014-02-02 13:19:14 -0500

    test

--- github.php
+++ github.php
@@ -15,6 +15,6 @@ file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
 if ($payload->ref === 'refs/heads/master')
 {
   // path to your site deployment script
-  exec('./build.sh');
+  exec('sudo su && cd /var/www/html && git pull origin master');
 }
 ?>
