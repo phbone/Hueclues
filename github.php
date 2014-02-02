dc17a32c87964ac4d51e0df2c0a159fc2bcde3f4
Bryan <bryanwan23@gmail.com>
2014-02-02 14:17:42 -0500

    hueclues

--- github.php
+++ github.php
@@ -15,7 +15,7 @@
 	);
  
 	// Run the commands for output
-	$output = '';
+	$output = 'test';
 	foreach($commands AS $command){
 		// Run it
 		$tmp = shell_exec($command);
@@ -26,3 +26,21 @@
  
 	// Make it pretty for manual user access (and why not?)
 ?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>GIT DEPLOYMENT SCRIPT</title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<pre>
+ .  ____  .    ____________________________
+ |/      \|   |                            |
+[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
+ |___==___|  /              &copy; oodavid 2012 |
+              |____________________________|
+ 
+<?php echo $output; ?>
+</pre>
+</body>
+</html>
