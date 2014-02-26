1b2b40ea6e6996de48b7d23fb5712e7eaec94385
Bryan <bryanwan23@gmail.com>
2014-02-26 18:03:45 -0500

    quick push

--- feed.php
+++ feed.php
@@ -170,6 +170,10 @@ $friend_array[] = $userid;
                 margin-bottom:25px;
                 font-size:20px;
             }
+            .topLabel i.fa{
+                position:absolute;
+                right:20px;
+            }
         </style>
     </head>
     <body>
@@ -177,7 +181,7 @@ $friend_array[] = $userid;
         <?php commonHeader(); ?>
         <div class="mainContainer" id="feed">
 
-            <div  id="feedLabel" class="topLabel" style='opacity:0.7;'><span id="topText" onclick="feedTrendToggle('feed')" >CLOSETS I FOLLOW</span><i class='fa fa-caret-right'></i></div>
+            <div id="feedLabel" class="topLabel" style='opacity:0.7;'><span id="topText" onclick="feedTrendToggle('feed')" >CLOSETS I FOLLOW</span><i class='fa fa-caret-right'></i></div>
             <div id="trendingLabel" class="topLabel" style="top:210px;" onclick="feedTrendToggle('trending')"><span id="topText">WHAT'S BUZZING</span><i class='fa fa-caret-down'></i></div>
 
             <div id="feedButtons" style="display:none;">
