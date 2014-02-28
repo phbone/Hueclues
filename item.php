9ab5cf2380c9a6298a0d1db8764aef528ed409f8
Bryan <bryanwan23@gmail.com>
2014-02-28 02:44:46 -0500

    quick push

---
+++ item.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+include('connection.php');
+include('database_functions.php');
+include('global_tools.php');
+include('global_objects.php');
+
+$userid = $_SESSION['userid'];
+$user = database_fetch("user", "userid", $userid);
+
+$itemid = $_GET['itemid'];
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php initiateTools() ?>
+        <link rel="stylesheet" type="text/css" href="/css/closet.css" />
+        <script type="text/javascript" >
+
+<?php initiateTypeahead(); ?>
+<?php checkNotifications(); ?>
+
+
+        </script>
+    </head>
+    <body>
+        <?php initiateNotification() ?>
+        <?php commonHeader() ?>
+        <img src="/img/loading.gif" id="loading"/>
+        <div class="mainContainer">
+
+            <div id="itemBackground"> 
+
+                <?php formatItem($itemid);?>
+
+            </div>
+
+        </div>
+    </body>
+</html>
