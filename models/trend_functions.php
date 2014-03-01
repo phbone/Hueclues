8ee5dbd52561a54d924c81143af72f667ccedb76
Bryan <bryanwan23@gmail.com>
2014-02-28 19:20:01 -0500

    quick push

--- models/trend_functions.php
+++ models/trend_functions.php
@@ -18,6 +18,7 @@ function hueCount($itemList) {
                 $hexcode = $itemList[$i];
                 echo $hexcode."<br/>";
                 list($h, $s, $l) = hex_2_hsl($hexcode);
+                echo $h;
                 if (array_key_exists($h, $colors)) {
                     $colors[$h]++;
                 } else {
