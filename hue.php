1ce815b98d6060a0e0f4378d98bf765dc2958fda
Bryan <bryanwan23@gmail.com>
2014-03-03 18:38:23 -0500

    quick push

--- hue.php
+++ hue.php
@@ -24,7 +24,10 @@ if ($loggedIn) {
     $emptyMessage = "<br/><br/>Want More Matches?<br/>Invite Friends";
     $item = database_fetch("item", "itemid", $itemid);
     $time = time();
-    database_insert("notification", "userid", $item['userid'], "from_user", $userid, "itemid", $itemid, "type", "2", "time", $time);
+    if ($item) {
+        echo "yes";
+        database_insert("notification", "userid", $item['userid'], "from_user", $userid, "itemid", $itemid, "type", "2", "time", $time);
+    }
 }
 
 function cmp($a, $b) {
