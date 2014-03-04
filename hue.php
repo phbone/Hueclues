b556621b45b608742225613d0dca64bcdc212217
Bryan <bryanwan23@gmail.com>
2014-03-03 19:11:31 -0500

    quick push

--- hue.php
+++ hue.php
@@ -9,9 +9,6 @@ $userid = $_SESSION['userid'];
 $loggedIn = isset($userid);
 
 
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
 $time = time();
 $itemid = $_GET['itemid'];
 $itemObject = returnItem($itemid);
