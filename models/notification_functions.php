7219154dc8770ad5fb7c7a0f5863b45be8964ba2
Bryan <bryanwan23@gmail.com>
2014-03-06 20:32:39 -0500

    quick push

--- models/notification_functions.php
+++ models/notification_functions.php
@@ -1,4 +1,5 @@
 <?php
+
 function formatNotification($notificationid) {
     // takes in a notificationid and formats the notification
 
@@ -12,22 +13,25 @@ function formatNotification($notificationid) {
     $item = database_fetch("item", "itemid", $itemid);
     $itemDesc = $item['description'];
     $seen = $notification['seen'];
-    
-    
+
+    if ($itemid) {
+        $itemObject = returnItem($itemid);
+    }
+
 
     if ($type == "0") {
         // notify user for liking an item
-        $message = "<a class='notificationBox' href='/item/$itemid'><img src='".$fromUser['picture']."' class='notificationPicture' />$fromUsername <br/> liked your item</a>";
+        $message = "<a class='notificationBox' href='/item/$itemid'><img src='" . $itemObject->image_link . "' class='notificationPicture' />$fromUsername <br/> liked your item</a>";
     } else if ($type == "1") {
         //notify user for following
-        $message = "<a class='notificationBox' href='/closet/$fromUsername'><img src='".$fromUser['picture']."' class='notificationPicture' />$fromUsername <br/>is now following you</a>";
+        $message = "<a class='notificationBox' href='/closet/$fromUsername'><img src='" . $fromUser['picture'] . "' class='notificationPicture' />$fromUsername <br/>is now following you</a>";
     } else if ($type == "2") {
         //notify user for matching
-        $message = "<a class='notificationBox' href='/hue/$itemid'><img src='".$fromUser['picture']."' class='notificationPicture' />$fromUsername <br/> matched your item</a>";
+        $message = "<a class='notificationBox' href='/hue/$itemid'><img src='" . $itemObject->image_link . "' class='notificationPicture' />$fromUsername <br/> matched your item</a>";
     } else if ($type == "3") {
         //notify user item used in outfit
         $outfitid = $itemid;
-        $message = "<a class='notificationBox' href='/outfit/$outfitid'><img src='".$fromUser['picture']."' class='notificationPicture' />$fromUsername <br/> is using your item in an outfit</a>";
+        $message = "<a class='notificationBox' href='/outfit/$outfitid'><img src='" . $itemObject->image_link . "' class='notificationPicture' />$fromUsername <br/> is using your item in an outfit</a>";
     }
     echo $message;
 }
