bf07a2e0b91266478ec06761bcd020a1fb2da0b4
Bryan <bryanwan23@gmail.com>
2014-03-06 21:11:14 -0500

    quick push

--- closet.php
+++ closet.php
@@ -2,7 +2,7 @@
 session_start();
 include('connection.php');
 include('database_functions.php');
-
+include('global_tools.php');
 $userid = $_SESSION['userid'];
 $user = database_fetch("user", "userid", $userid);
 // your userid
@@ -10,14 +10,11 @@ $owner_username = $_GET['username'];
 $owner = database_fetch("user", "username", $owner_username);
 $closet_owner = $owner['userid'];
 //// userid of the person whose closet your trying to see
-if (!$userid && !$owner_username) {
-    header("Location:http://hueclues.com");
-}
 $owns_closet = ($userid == $closet_owner);
 $item_count = $owner['itemcount'];
 $useridArray[] = $owner['userid'];
 $view = $_GET['view'];
-include('global_tools.php');
+
 $size = getimagesize($owner['picture']);
 ?>
 <!DOCTYPE html>
