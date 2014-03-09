09630c5b58ea66852ed2f2b936ecb6a1da5da0ea
Bryan <bryanwan23@gmail.com>
2014-03-08 23:14:55 -0500

    quick push

--- imagescript.php
+++ imagescript.php
@@ -9,11 +9,11 @@ include('global_objects.php');
 $query = "SELECT * FROM item WHERE itemid > 0";
 $rst = mysql_query($query);
 while($item = mysql_fetch_array($rst)){
-    echo $item['itemid'];
     $itemObject = returnItem($item['itemid']);
     
     list($width, $height) = getimagesize($itemObject->image_link);
     $sizeRatio = $width/$height;
+    echo $sizeRatio . "<br/>";
     database_update("item", "itemid", $item['itemid'], "","", "sizeRatio", $sizeRatio);
 }
 ?>
