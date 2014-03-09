20eb5b181e9c65747ddfba396675963eabbeda3d
Bryan <bryanwan23@gmail.com>
2014-03-08 23:03:54 -0500

    quick push

--- imagescript.php
+++ imagescript.php
@@ -10,8 +10,10 @@ $query = "SELECT * FROM item WHERE 1";
 $rst = mysql_query($query);
 while($item = mysql_fetch_array($rst)){
     $itemObject = returnItem($item['itemid']);
+    
+    echo $item['itemid'];
     list($width, $height) = getimagesize($itemObject->image_link);
     $sizeRatio = $width/$height;
-    database_update("item", "itemid", $item['itemid'], "","", "sizeRatio", $sizeRatio);
+    //database_update("item", "itemid", $item['itemid'], "","", "sizeRatio", $sizeRatio);
 }
 ?>
