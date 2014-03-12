c760ea9c23d9d87bafcddc48bc859e64a2576d92
Bryan <bryanwan23@gmail.com>
2014-03-11 23:20:38 -0400

    putting imagelink formatting into imagescript

--- imagescript.php
+++ imagescript.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 include('connection.php');
 include('database_functions.php');
@@ -8,14 +9,26 @@ include('global_objects.php');
 
 $query = "SELECT * FROM item WHERE itemid > 0";
 $rst = mysql_query($query);
-while($item = mysql_fetch_array($rst)){
+while ($item = mysql_fetch_array($rst)) {
+
+/*
+// recalculates all the item size ratios
     $itemObject = returnItem($item['itemid']);
-    
     list($width, $height) = getimagesize($itemObject->image_link);
-    $sizeRatio = round($width/$height,2);
-    if($sizeRatio == 0){
+    $sizeRatio = round($width / $height, 2);
+    if ($sizeRatio == 0) {
         $sizeRatio = 1;
     }
-    database_update("item", "itemid", $item['itemid'], "","", "sizeRatio", $sizeRatio);
+    database_update("item", "itemid", $item['itemid'], "", "", "sizeRatio", $sizeRatio);
+*/
+    
+    
+    // adds http to all links
+    if (strpos($item['purchaselink'], 'http') !== false) {
+        // do nothing already has http
+    } else {
+        $link = substr_replace($item['purchaselink'], 'http://', 0, 0);
+        database_update("item", "itemid", $item['itemid'], "", "", "purchaselink", $link);
+    }
 }
 ?>
