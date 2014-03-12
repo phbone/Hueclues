7d1a63d26aa9123d33d987353a45cdf41aea320e
Bryan <bryanwan23@gmail.com>
2014-03-11 23:24:07 -0400

    quick push

--- imagescript.php
+++ imagescript.php
@@ -11,24 +11,31 @@ $query = "SELECT * FROM item WHERE itemid > 0";
 $rst = mysql_query($query);
 while ($item = mysql_fetch_array($rst)) {
 
-/*
-// recalculates all the item size ratios
-    $itemObject = returnItem($item['itemid']);
-    list($width, $height) = getimagesize($itemObject->image_link);
-    $sizeRatio = round($width / $height, 2);
-    if ($sizeRatio == 0) {
-        $sizeRatio = 1;
-    }
-    database_update("item", "itemid", $item['itemid'], "", "", "sizeRatio", $sizeRatio);
-*/
-    
-    
+    /*
+      // recalculates all the item size ratios
+      $itemObject = returnItem($item['itemid']);
+      list($width, $height) = getimagesize($itemObject->image_link);
+      $sizeRatio = round($width / $height, 2);
+      if ($sizeRatio == 0) {
+      $sizeRatio = 1;
+      }
+      database_update("item", "itemid", $item['itemid'], "", "", "sizeRatio", $sizeRatio);
+     */
+
+
     // adds http to all links
-    if (strpos($item['purchaselink'], 'http') !== false) {
-        // do nothing already has http
-    } else {
-        $link = substr_replace($item['purchaselink'], 'http://', 0, 0);
-        database_update("item", "itemid", $item['itemid'], "", "", "purchaselink", $link);
+    if ($item['purchaselink']) { // make sure there is a link
+        // if link exists and doesn't have http add it
+        if (strpos($item['purchaselink'], 'http') !== false) {
+            // delete any empty find links
+            if($item['purchaselink']=="http://"){
+                database_update("item", "itemid", $item['itemid'], "", "", "purchaselink", "");
+            }
+            // do nothing already has http
+        } else {
+            $link = substr_replace($item['purchaselink'], 'http://', 0, 0);
+            database_update("item", "itemid", $item['itemid'], "", "", "purchaselink", $link);
+        }
     }
 }
 ?>
