7caba31d040a5fa8c7f22e1fe53f5772218dec4f
Fady Shoukry <fady.shoukry@mail.utoronto.ca>
2014-03-27 23:47:30 -0400

    quick push

--- testing/recent_items.php
+++ testing/recent_items.php
@@ -48,9 +48,9 @@ $userid = $_SESSION['userid'];
                         
                         $hex = $item['code'];
                         // Convert the hex color into a 6 bit string
-                        $r = strval(round(hexdec(substr($hex, 0, 2))*(7/255)));
-                        $g = strval(round(hexdec(substr($hex, 2, 2))*(7/255)));
-                        $b = strval(round(hexdec(substr($hex, 4, 2))*(7/255)));
+                        $r = strval(round(hexdec(substr($hex, 0, 2))*(7/255), 0, PHP_ROUND_HALF_DOWN));
+                        $g = strval(round(hexdec(substr($hex, 2, 2))*(7/255), 0, PHP_ROUND_HALF_DOWN));
+                        $b = strval(round(hexdec(substr($hex, 4, 2))*(7/255), 0, PHP_ROUND_HALF_DOWN));
                         $color6bit = $r . $g . $b;
                         
                         $R = ($color6bit[0] == '0')? '00' : dechex(intval($color6bit[0])*(255/7));
