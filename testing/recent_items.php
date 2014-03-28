e1090ad58cd6479e09820a3068a0e878ea611b4b
Fady Shoukry <fady.shoukry@mail.utoronto.ca>
2014-03-27 21:33:53 -0400

    quick push

--- testing/recent_items.php
+++ testing/recent_items.php
@@ -49,8 +49,8 @@ $userid = $_SESSION['userid'];
                         $hex = $item['code'];
                         // Convert the hex color into a 6 bit string
                         $r = strval(round(hexdec(substr($hex, 1, 2))*(3/255)));
-                        $g = strval(round(hexdec(substr($hex, 3, 2))*(3/255)));
-                        $b = strval(round(hexdec(substr($hex, 5, 2))*(3/255)));
+                        $g = strval(round(hexdec(substr($hex, 2, 2))*(3/255)));
+                        $b = strval(round(hexdec(substr($hex, 4, 2))*(3/255)));
                         $color6bit = $r . $g . $b;
                         
                         $R = ($color6bit[0] == '0')? '00' : dechex(intval($color6bit[0])*(255/3));
